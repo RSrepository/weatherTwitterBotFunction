80ac3f56dc2655831bdb4a4afc39b1abb2c67afa
斉木良介 <ryosuke.saiki@dalcomsoft.co.jp>
2021-12-30 13:17:27 +0900

    Lambdaでテストツイート成功バージョン

--- lambda_function.py
+++ lambda_function.py
@@ -1,21 +1,17 @@
+import sys
+sys.path.append('python_package')
 # coding: UTF-8
-import tweepy
 import json
+import tweepy
 
 def lambda_handler(event, context):
-    try:
-        # 設定ファイル読み込み
-config = json.load(open('config.json', 'r'))
+    # 設定ファイル読み込み
+    config = json.load(open('config.json', 'r'))
 
-# Twitterオブジェクト設定
-client = tweepy.Client(config.twitter.bearerToken, config.twitter.consumerKey, config.twitter.consumerSecret, config.twitter.accessToken, config.twitter.accessTokenSecret)
 
-# ツイート
-client.create_tweet(text="Lambdaからテストツイート")
-        return {
-        'statusCode': 200,
-        'body': json.dumps('success')
-    }
-except: Exception
-    raise Exception
+    # Twitterオブジェクト設定
+    client = tweepy.Client(config['twitter']['bearerToken'], config['twitter']['consumerKey'],
+                           config['twitter']['consumerSecret'], config['twitter']['accessToken'], config['twitter']['accessTokenSecret'])
 
+    # ツイート
+    client.create_tweet(text="Lambdaからテストツイート")
